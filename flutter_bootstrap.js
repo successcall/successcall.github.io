bc49006281dfb8304ada04e66189248ee198891d
Musab Ibnu Siraj <musab.dot@gmail.com>
2025-12-03 11:23:47 +0530

    Bump build number to 25 in version.json for the successcall app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1010378049"
+    serviceWorkerVersion: "2251462965"
   }
 });
