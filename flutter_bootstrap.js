c644e1c3e741388d2cbdb702bf48bc4e9de19085
Musab Ibnu Siraj <musab.dot@gmail.com>
2025-12-05 07:52:08 +0530

    Update service worker version and resource hashes for improved caching

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1517093463"
+    serviceWorkerVersion: "3197769660"
   }
 });
