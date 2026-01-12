87e22d3415264c3fc1e7fbc8960de58b54dcf644
Musab Ibnu Siraj <musab.dot@gmail.com>
2026-01-13 01:55:02 +0530

    Update version and start URL in manifest and version files

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2388951628"
+    serviceWorkerVersion: "3997410982"
   }
 });
