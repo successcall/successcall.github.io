20ca73f0d78683d9c5f6b364f9c062b75685ebda
Musab Ibnu Siraj <musab.dot@gmail.com>
2026-08-05 16:02:06 +0530

    asd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2324488537" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1922589124" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
